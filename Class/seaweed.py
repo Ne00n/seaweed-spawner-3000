fdf7ab70358a36ca4080cdbfa67d54e64939a622
Neoon <neo@tiefkuehler-madness.me>
2021-09-08 11:11:36 +0200

    retry ssh commands

--- Class/seaweed.py
+++ Class/seaweed.py
@@ -10,12 +10,19 @@ class SeaweedFS:
         with open(config) as handle:
             self.targets = json.loads(handle.read())
 
-    def cmd(self,server,command,interactive):
+    def cmd(self,server,command):
         cmd = ['ssh','root@'+server,command]
-        if interactive == True:
-            return subprocess.check_output(cmd).decode("utf-8")
-        else:
-            subprocess.run(cmd)
+        for run in range(4):
+            try:
+                p = subprocess.run(cmd, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, timeout=300)
+                if p.returncode != 0:
+                    print("Warning got returncode",p.returncode,"on",server)
+                    print("Error:",p.stderr.decode('utf-8'))
+                if p.returncode != 255: return [p.stdout.decode('utf-8'),p.stderr.decode('utf-8')]
+            except Exception as e:
+                print("Error:",e)
+            print("Retrying",cmd,"on",server)
+            time.sleep(random.randint(5, 15))
 
     def prepare(self,server,data,delete=False,terminate=False):
         print("---",server,"Preparing","---")
@@ -26,9 +33,9 @@ class SeaweedFS:
         #Disable old configs
         for service in parsed:
             print("Stopping "+service)
-            self.cmd(data['ip'],'systemctl stop '+service+' && systemctl disable '+service,False)
-            if delete: self.cmd(data['ip'],'rm /etc/systemd/system/'+service,False)
-        if terminate: self.cmd(data['ip'],'userdel -r seaweedfs',False)
+            self.cmd(data['ip'],'systemctl stop '+service+' && systemctl disable '+service)
+            if delete: self.cmd(data['ip'],'rm /etc/systemd/system/'+service)
+        if terminate: self.cmd(data['ip'],'userdel -r seaweedfs')
 
 
     def clean(self):
@@ -47,22 +54,22 @@ class SeaweedFS:
         T = Templator()
         url = "https://github.com/chrislusf/seaweedfs/releases/download/"+self.targets['version']+"/linux_"+data['type']+".tar.gz"
         print("Installing SeaweedFS")
-        self.cmd(data['ip'],'cd /tmp/; wget '+url+"; tar xvf linux_"+data['type']+".tar.gz; mv weed /usr/local/bin/; rm linux_"+data['type']+".tar.gz;",False)
+        self.cmd(data['ip'],'cd /tmp/; wget '+url+"; tar xvf linux_"+data['type']+".tar.gz; mv weed /usr/local/bin/; rm linux_"+data['type']+".tar.gz;")
 
         print("Adding non privileged user for SeaweedFS")
-        self.cmd(data['ip'],'getent passwd seaweedfs &>/dev/null && echo "Skipping" ||  mkdir /home/seaweedfs/ && mkdir /home/seaweedfs/master && mkdir /home/seaweedfs/volume && useradd seaweedfs -r -d /home/seaweedfs -s /bin/false && chown -R seaweedfs:seaweedfs /home/seaweedfs/ && chmod -R 700 /home/seaweedfs/',False)
+        self.cmd(data['ip'],'getent passwd seaweedfs &>/dev/null && echo "Skipping" ||  mkdir /home/seaweedfs/ && mkdir /home/seaweedfs/master && mkdir /home/seaweedfs/volume && useradd seaweedfs -r -d /home/seaweedfs -s /bin/false && chown -R seaweedfs:seaweedfs /home/seaweedfs/ && chmod -R 700 /home/seaweedfs/')
 
         print('Creating & Starting SeaweedFS master systemd service')
         config = T.genSystemd('master',data['vpn'],9333,'mdir','peers',9333,self.targets)
-        self.cmd(data['ip'],'echo "'+config+'" > /etc/systemd/system/SeaweedFSmaster.service && systemctl enable SeaweedFSmaster && systemctl start SeaweedFSmaster',False)
+        self.cmd(data['ip'],'echo "'+config+'" > /etc/systemd/system/SeaweedFSmaster.service && systemctl enable SeaweedFSmaster && systemctl start SeaweedFSmaster')
 
         print('Creating & Starting SeaweedFS volume systemd service')
         config = T.genSystemd('volume',data['vpn'],9433,'dir','mserver',9333,self.targets,data['dc'],data['rack'])
-        self.cmd(data['ip'],'echo "'+config+'" > /etc/systemd/system/SeaweedFSvolume.service && systemctl enable SeaweedFSvolume && systemctl start SeaweedFSvolume',False)
+        self.cmd(data['ip'],'echo "'+config+'" > /etc/systemd/system/SeaweedFSvolume.service && systemctl enable SeaweedFSvolume && systemctl start SeaweedFSvolume')
 
         print('Creating & Starting SeaweedFS filer systemd service')
         config = T.genSystemd('filer',data['vpn'],9533,'dir','master',9333,self.targets)
-        self.cmd(data['ip'],'echo "'+config+'" > /etc/systemd/system/SeaweedFSfiler.service && systemctl enable SeaweedFSfiler && systemctl start SeaweedFSfiler',False)
+        self.cmd(data['ip'],'echo "'+config+'" > /etc/systemd/system/SeaweedFSfiler.service && systemctl enable SeaweedFSfiler && systemctl start SeaweedFSfiler')
 
     def run(self):
         print("Launching")
