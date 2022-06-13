e41978010f6af1bf464c64927d345b0e66e7de94
Neoon <neo@tiefkuehler-madness.me>
2022-06-13 16:34:29 +0200

    add mount option +filer fix

--- Class/templator.py
+++ Class/templator.py
@@ -8,34 +8,37 @@ class Templator:
         return peers
 
     def genSystemd(self,type,ip,port,dirType,peerType,peerPort,targets,dc=False,rack=False):
-        template = '''
-        [Unit]
-        Description=SeaweedFS '''+type+'''
-        After=network.target
-        StartLimitBurst=3
-        StartLimitIntervalSec=200
+        user = "root" if type == "mount" else "seaweedfs"
+        template = f'''[Unit]
+Description=SeaweedFS {type}
+After=network.target
+StartLimitBurst=3
+StartLimitIntervalSec=200
 
-        [Service]
-        Type=simple
-        User=seaweedfs
-        Group=seaweedfs
-        Restart=on-failure
-        RestartSec=60s
+[Service]
+Type=simple
+User={user}
+Group=seaweedfs
+Restart=on-failure
+RestartSec=60s
 
-        ExecStartPre=/bin/sh -c 'until ping -c1 '''+ip+'''; do sleep 1; done;'
-        ExecStart='''
+ExecStartPre=/bin/sh -c 'until ping -c1 '''+ip+'''; do sleep 1; done;'
+ExecStart='''
 
         if type == "master":
             template += '/usr/local/bin/weed '+type+' -defaultReplication='+str(targets['replica'])+' -ip='+ip+' -ip.bind='+ip+' -port='+str(port)+' -'+dirType+'=/home/seaweedfs/'+type+' -'+peerType+'='+self.genPeers(peerPort,targets)
         elif type == "filer":
-            template += '/usr/local/bin/weed '+type+' -ip='+ip+' -ip.bind='+ip+' -port='+str(port)+' -'+peerType+'='+self.genPeers(peerPort,targets)
+            template += f'/usr/local/bin/weed filer -localSocket=/home/seaweedfs/filer.sock -ip={ip} -ip.bind={ip} -port={port} -{peerType}={self.genPeers(peerPort,targets)}'
+        elif type == "mount":
+            template += f'''/usr/local/bin/weed mount -filer={self.genPeers(port,targets)} -dir={dc} -filer.path={rack}
+ExecStop=/usr/bin/umount -l {dc}'''
         else:
             template += '/usr/local/bin/weed '+type+' -ip='+ip+' -ip.bind='+ip+' -port='+str(port)+' -'+dirType+'=/home/seaweedfs/'+type+' -dataCenter='+dc+' -rack='+rack+' -'+peerType+'='+self.genPeers(peerPort,targets)
 
-        template += '''
-        WorkingDirectory=/home/seaweedfs/
-        SyslogIdentifier=seaweedfs-'''+type+'''
+        template += f'''
+WorkingDirectory=/home/seaweedfs/
+SyslogIdentifier=seaweedfs-{type}
 
-        [Install]
-        WantedBy=multi-user.target'''
+[Install]
+WantedBy=multi-user.target'''
         return template
