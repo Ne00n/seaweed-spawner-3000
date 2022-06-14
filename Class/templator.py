2964bb0a8687fb6d25615fb11d880962c1e0ae7d
Neoon <neo@tiefkuehler-madness.me>
2022-06-14 17:15:19 +0200

    add disableHttp option

--- Class/templator.py
+++ Class/templator.py
@@ -8,6 +8,7 @@ class Templator:
         return peers
 
     def genSystemd(self,type,ip,port,dirType,peerType,peerPort,targets,dc=False,rack=False):
+        disableHttp = "-disableHttp" if targets['disableHttp'] else ""
         user = "root" if type == "mount" else "seaweedfs"
         template = f'''[Unit]
 Description=SeaweedFS {type}
@@ -26,9 +27,9 @@ ExecStartPre=/bin/sh -c 'until ping -c1 '''+ip+'''; do sleep 1; done;'
 ExecStart='''
 
         if type == "master":
-            template += '/usr/local/bin/weed '+type+' -defaultReplication='+str(targets['replica'])+' -ip='+ip+' -ip.bind='+ip+' -port='+str(port)+' -'+dirType+'=/home/seaweedfs/'+type+' -'+peerType+'='+self.genPeers(peerPort,targets)
+            template += f'/usr/local/bin/weed master {disableHttp} -defaultReplication={targets["replica"]} -ip={ip} -ip.bind={ip} -port={port} -{dirType}=/home/seaweedfs/{type} -{peerType}={self.genPeers(peerPort,targets)}'
         elif type == "filer":
-            template += f'/usr/local/bin/weed filer -localSocket=/home/seaweedfs/filer.sock -ip={ip} -ip.bind={ip} -port={port} -{peerType}={self.genPeers(peerPort,targets)}'
+            template += f'/usr/local/bin/weed filer {disableHttp} -localSocket=/home/seaweedfs/filer.sock -ip={ip} -ip.bind={ip} -port={port} -{peerType}={self.genPeers(peerPort,targets)}'
         elif type == "mount":
             template += f'''/usr/local/bin/weed mount -filer={self.genPeers(port,targets)} -dir={dc} -filer.path={rack}
 ExecStop=/usr/bin/umount -l {dc}'''
